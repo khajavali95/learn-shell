0ebb59f96d3ddf1bd929e3c494c668d84742d95a
khajavali <khajavali1995@gmail.com>
2023-01-03 21:59:11 +0530

    shell print.sh 1

---
+++ print.sh
@@ -0,0 +1,7 @@
+echo "hello world"
+
+echo -e "\e[31m Colour Change\e[0m"
+echo -e "\e[32m Colour Change\e[0m"
+echo -e "\e[33m Colour Change\e[0m"
+echo -e "\e[34m Colour Change\e[0m"
+echo -e "\e[35m Colour Change\e[0m"
