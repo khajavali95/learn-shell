cdb7c0af75e8fa6d4b12cc09a2632f8dfcc1e90d
khajavali <khajavali1995@gmail.com>
2023-01-03 22:10:57 +0530

    shell print.sh1

--- print.sh
+++ print.sh
@@ -1,9 +1,9 @@
 echo "hello world"
 
-echo -e "\e[31mColour Change\e[0m"
-echo -e "\e[32mColour Change\e[0m"
-echo -e "\e[33mColour Change\e[0m"
-echo -e "\e[34mColour Change\e[0m"
-echo -e "\e[35mColour Change\e[0m"
+echo -e "\e[31mCOLOUR\e[0m"
+echo -e "\e[32mCOLOUR\e[0m"
+echo -e "\e[33mCOLOUR\e[0m"
+echo -e "\e[34mCOLOUR\e[0m"
+echo -e "\e[35mCOLOUR\e[0m"
 
 echo "bye"
